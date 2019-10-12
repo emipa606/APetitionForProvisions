aac8639e185238ca4509d1e9e29ad80ac1d608d2
Trevor <tkhowell95@gmail.com>
2019-10-12 16:57:08 -0600

    Add project files.

---
+++ Source/ItemRequests.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System;
+using RimWorld;
+using UnityEngine;
+using RimWorld.Planet;
+using Verse;
+using Harmony;
+
+namespace ItemRequests
+{
+    [StaticConstructorOnStartup]
+    public class ItemRequests
+    {
+        static ItemRequests()
+        {
+            var harmony = HarmonyInstance.Create("com.github.toywalrus.itemrequests");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
+
+// Faction: GetInfoText() GetCallLabel() 
+// Dialog_Negotiation: DoWindowContents()
+// Dialog_NodeTree
+// Verse.Window
